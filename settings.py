fd2af54baae5728e43afd872aecd39105b583c1d
efischer19 <efischer0119@gmail.com>
2017-07-02 15:52:45 -0400

    kill composeexample, add settings template
    Working docker image at efischer19/learnball_web:debug_works
    composeexample taken from https://docs.docker.com/compose/django/

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnball',
+        'HOST': 'db',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'demo'
+    }
+}
